2bf55ad17f04bb7950f0009a6d2e5eea2a3ee810
Vladimir <pharmdoktor1972@icloud.com>
2025-10-23 22:48:17 +0300

    Delete minecraft-protobuf-fabric-demo directory

--- minecraft-protobuf-fabric-demo/src/main/java/com/rolloerro/minecraftprotobufdemo/NetworkingConstants.java
+++
@@ -1,7 +0,0 @@
-package com.rolloerro.minecraftprotobufdemo;
-
-import net.minecraft.util.Identifier;
-
-public class NetworkingConstants {
-    public static final Identifier SEND_MESSAGE_PACKET = new Identifier(MinecraftProtobufDemo.MOD_ID, "send_message");
-}
